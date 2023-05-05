bd4ecbf6636e65f30937cf298972ff44ecd61922
Henok <henokmicrometre@gamil.com>
2023-05-05 13:07:07 +0100

    added tensorflow/

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pip install numpy pandas scipy  matplotlib jupyterlab
+pip install numpy pandas scipy  matplotlib jupyterlab tensorflow-cpu keras
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
