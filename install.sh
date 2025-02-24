7cde777fe9304083ea40606776438a8d27b3c757
ubuntu-desktop <ubuntu-desktop@gmail.com>
2025-02-24 02:05:03 +0000

    adding js

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install numpy pandas scipy  matplotlib jupyterlab tensorflow-cpu keras
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+#pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2.4
+
+pip install numpy pandas scikit-learn scipy  matplotlib jupyterlab #tensorflow-cpu keras
